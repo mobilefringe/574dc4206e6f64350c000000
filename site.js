3eaea7aefaadb24b9cebc504d65743937551f189
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-31 14:21:54 -0400

    Chino Spectrum Towne Center 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Chino Spectrum Towne Center",
                     "default_image" : "http://assets.codecloudapp.com/sites/574dc4206e6f64350c000000/image/jpeg/undefined/cstc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196558634"
+                    "instagram" : "215518758"
                 }
