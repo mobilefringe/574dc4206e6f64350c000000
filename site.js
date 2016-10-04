9060c8d7717c64a83a390a57085d848e52af1e44
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 17:41:04 -0400

    Chino Spectrum Towne Center 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Chino Spectrum Towne Center",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f41f2d6e6f647c36030000/image/jpeg/undefined/cstc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//chinospectrum.mallmaverick.com/api/v2/chinospectrum/social.json"
+                }
